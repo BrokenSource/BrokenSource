2710e6d151009133e274e90817d954d4b84e30c1
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2023-11-24 21:59:38 -0400

    Better namespace for BrokenSmart.load_image, update submodules

--- Broken/BrokenUtils.py
+++ Broken/BrokenUtils.py
@@ -195,6 +195,10 @@ class BrokenRelay:
         """Adds callbacks to the list of callables, runs on self.__call__"""
         return self.__bind__(callbacks)
 
+    def subscribe(self, *callbacks: callable) -> Self:
+        """Adds callbacks to the list of callables, runs on self.__call__"""
+        return self.__bind__(callbacks)
+
     def __matmul__(self, *callbacks: callable) -> Self:
         """Convenience syntax for binding"""
         return self.__bind__(callbacks)
@@ -392,6 +396,37 @@ class BrokenUtils:
                 return base
 
         return extender
+
+    def load_image(image: PilImage | PathLike | URL, pixel="RGB", cache=True, echo=True) -> Option[PilImage, None]:
+        """Smartly load 'SomeImage', a path, url or PIL Image"""
+
+        # Nothing to do if already a PIL Image
+        if isinstance(image, PilImage):
+            return image
+
+        try:
+            # Load image if a path or url is supplied
+            if any([isinstance(image, T) for T in (PathLike, str)]):
+                if (path := BrokenPath.true_path(image)).exists():
+                    log.info(f"Loading image from Path [{path}]", echo=echo)
+                    return PIL.Image.open(path).convert(pixel)
+                else:
+                    log.info(f"Loading image from (maybe) URL [{image}]", echo=echo)
+                    try:
+                        requests = BROKEN_REQUESTS_CACHE if cache else requests
+                        return PIL.Image.open(BytesIO(requests.get(image).content)).convert(pixel)
+                    except Exception as e:
+                        log.error(f"Failed to load image from URL or Path [{image}]: {e}", echo=echo)
+                        return None
+            else:
+                log.error(f"Unknown image parameter [{image}], must be a PIL Image, Path or URL", echo=echo)
+                return None
+
+        # Can't open file
+        except Exception as e:
+            log.error(f"Failed to load image [{image}]: {e}", echo=echo)
+            return None
+
 
 # -------------------------------------------------------------------------------------------------|
 
@@ -822,34 +857,3 @@ class BrokenTyper:
 # Relics from the past
 
 BROKEN_REQUESTS_CACHE = requests_cache.CachedSession(BROKEN_DIRECTORIES.CACHE/'RequestsCache')
-
-class BrokenSmart:
-    def load_image(image: Union[PilImage, PathLike, URL], pixel="RGB", cache=True, echo=True) -> Option[PilImage, None]:
-        """Smartly load 'SomeImage', a path, url or PIL Image"""
-
-        # Nothing to do if already a PIL Image
-        if isinstance(image, PilImage):
-            return image
-
-        try:
-            # Load image if a path or url is supplied
-            if any([isinstance(image, T) for T in (PathLike, str)]):
-                if (path := BrokenPath.true_path(image)).exists():
-                    log.info(f"Loading image from Path [{path}]", echo=echo)
-                    return PIL.Image.open(path).convert(pixel)
-                else:
-                    log.info(f"Loading image from (maybe) URL [{image}]", echo=echo)
-                    try:
-                        requests = BROKEN_REQUESTS_CACHE if cache else requests
-                        return PIL.Image.open(BytesIO(requests.get(image).content)).convert(pixel)
-                    except Exception as e:
-                        log.error(f"Failed to load image from URL or Path [{image}]: {e}", echo=echo)
-                        return None
-            else:
-                log.error(f"Unknown image parameter [{image}], must be a PIL Image, Path or URL", echo=echo)
-                return None
-
-        # Can't open file
-        except Exception as e:
-            log.error(f"Failed to load image [{image}]: {e}", echo=echo)
-            return None
