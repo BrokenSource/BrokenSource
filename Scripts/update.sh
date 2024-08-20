d7602d24be26134281232aa3afbb56cdec9edac7
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2024-08-20 15:20:27 -0400

    Fix update.sh git pull recurse command

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -2,4 +2,4 @@
 cd "$(dirname "$0")"/..
 git submodule update --init --recursive --jobs 4
 git submodule foreach --recursive 'git checkout main || true'
-git pull --recursive --jobs 4
+git pull --recurse-submodules --jobs 4
