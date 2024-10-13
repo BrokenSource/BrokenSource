620f413b6e9ff9add50508a5d992061912bdc54b
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2024-10-13 18:46:37 -0400

    Bump internal development version

--- Broken/Version.py
+++ Broken/Version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-__version__ = "0.7.1"
+__version__ = "0.8.0.dev0"
 __all__ = ["__version__"]
 
 def export(**options) -> None:
@@ -8,8 +8,8 @@ def export(**options) -> None:
 
 if (__name__ == "__main__"):
     export(
-        BROKEN_VERSION=__version__,
-        BROKEN_PYAPP=True,
-        BROKEN_PYPI=True,
-        BROKEN_TAG=True,
+        GHA_VERSION=__version__,
+        GHA_PYAPP=False,
+        GHA_PYPI=False,
+        GHA_TAG=False,
     )
