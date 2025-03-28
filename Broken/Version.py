9be142f37f5950c5285d359902fbfba9d72ae3e1
Tremeschin <29046864+Tremeschin@users.noreply.github.com>
2025-03-28 01:23:08 -0400

    Refactor workflows to minimal parts

--- Broken/Version.py
+++ Broken/Version.py
@@ -4,10 +4,6 @@
 __version__ = "0.9.0.dev0"
 __options__ = dict(
     GHA_VERSION=__version__,
-    GHA_DOCKER=False,
-    GHA_PYAPP=False,
-    GHA_PYPI=False,
-    GHA_TAG=False,
 )
 
 # Export options to environment
